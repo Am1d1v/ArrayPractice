0b2b838f56c35796577c2e80207d4431c7123e77
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-06 15:25:03 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,13 +7,29 @@
 
 const dollarTransactions = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
 
+const newTransaction = [100, -200, 300, -400, 500, -600, 700, -800, 900, -1000];
+
 const usdToEuro = 0.91;
 
 const transactionEuro = dollarTransactions.map((transaction) => {
     return transaction * usdToEuro
 });
-
 console.log(transactionEuro);
 
 
+const euroArray = [];
 
+for( const transaction of dollarTransactions ){
+
+    euroArray.push(transaction * usdToEuro);
+};
+console.log(euroArray);
+
+
+const transactionInfo = newTransaction.map((transaction, index) => {
+        if(transaction > 0){
+            console.log(` Transaction №${index + 1}: ${transaction}$ was deposited`);
+        } else {
+            console.log(`Transaction №${index + 1}: ${transaction}$ was withdrew`);
+        }
+})
